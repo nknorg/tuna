d624d907a8574d8bc72744828d080c5396408a62
Nikolai Perevozchikov <trueinsider@gmail.com>
2019-03-11 06:43:45 +0300

    rework udp on client side

--- client/client.go
+++ client/client.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"unsafe"
 
 	. "github.com/nknorg/nkn-sdk-go"
 	"github.com/nknorg/nkn/vault"
@@ -22,11 +23,18 @@ type Configuration struct {
 }
 
 type TunnelClient struct {
-	config  Configuration
-	tcpConn map[byte]net.Conn
-	udpConn map[byte]net.Conn
-	session map[byte]*smux.Session
-	wallet  *WalletSDK
+	config      Configuration
+	tcpConn     map[byte]net.Conn
+	udpConn     map[byte]*net.UDPConn
+	serviceConn map[connKey]*net.UDPConn
+	clientAddr  map[uint16]*net.UDPAddr
+	session     map[byte]*smux.Session
+	wallet      *WalletSDK
+}
+
+type connKey struct {
+	serviceId byte
+	portId    byte
 }
 
 func NewTunnelClient() *TunnelClient {
@@ -47,7 +55,9 @@ func NewTunnelClient() *TunnelClient {
 	return &TunnelClient{
 		config,
 		make(map[byte]net.Conn),
-		make(map[byte]net.Conn),
+		make(map[byte]*net.UDPConn),
+		make(map[connKey]*net.UDPConn),
+		make(map[uint16]*net.UDPAddr),
 		make(map[byte]*smux.Session),
 		wallet,
 	}
@@ -60,37 +70,51 @@ func (tc *TunnelClient) Start() {
 			log.Panicln(err)
 		}
 		service := tuna.Services[serviceId]
-		tc.listenService(serviceId, 0, tuna.TCP, service.TCP)
-		tc.listenService(serviceId, len(service.TCP), tuna.UDP, service.UDP)
+		tc.listenTCP(serviceId, service.TCP)
+		tc.listenUDP(serviceId, len(service.TCP), service.UDP)
 	}
 }
 
-func (tc *TunnelClient) connectToNode(serviceId byte, protocol tuna.Protocol, force bool) (net.Conn, error) {
-	var conn map[byte]net.Conn
-	if protocol == tuna.TCP {
-		conn = tc.tcpConn
-	} else if protocol == tuna.UDP {
-		conn = tc.udpConn
+func (tc *TunnelClient) getServerTCPConn(serviceId byte, force bool) (net.Conn, error) {
+	err := tc.createServerConn(serviceId, force)
+	if err != nil {
+		return nil, err
 	}
-	if conn[serviceId] == nil || force {
+	return tc.tcpConn[serviceId], nil
+}
+
+func (tc *TunnelClient) getServerUDPConn(serviceId byte, force bool) (*net.UDPConn, error) {
+	err := tc.createServerConn(serviceId, force)
+	if err != nil {
+		return nil, err
+	}
+	return tc.udpConn[serviceId], nil
+}
+
+func (tc *TunnelClient) createServerConn(serviceId byte, force bool) error {
+	service := tuna.Services[serviceId]
+	hasTCP := len(service.TCP) > 0
+	hasUDP := len(service.UDP) > 0
+
+	if hasTCP && tc.tcpConn[serviceId] == nil || hasUDP && tc.udpConn[serviceId] == nil || force {
 		serviceName := tuna.Services[serviceId].Name
 
-	RandomBucket:
+		RandomBucket:
 		for {
 			lastBucket, err := tc.wallet.GetTopicBucketsCount(serviceName)
 			if err != nil {
-				return nil, err
+				return err
 			}
 			bucket := uint32(rand.Intn(int(lastBucket) + 1))
 			subscribers, err := tc.wallet.GetSubscribers(serviceName, bucket)
 			if err != nil {
-				return nil, err
+				return err
 			}
 			subscribersCount := len(subscribers)
 
 			subscribersIndexes := rand.Perm(subscribersCount)
 
-		RandomSubscriber:
+			RandomSubscriber:
 			for {
 				if len(subscribersIndexes) == 0 {
 					continue RandomBucket
@@ -105,39 +129,49 @@ func (tc *TunnelClient) connectToNode(serviceId byte, protocol tuna.Protocol, fo
 					}
 
 					addressParts := strings.Split(metadata, ",")
-					var address string
-					if protocol == tuna.TCP {
-						address = addressParts[0] + ":" + addressParts[1]
-					} else if protocol == tuna.UDP {
-						address = addressParts[0] + ":" + addressParts[2]
+
+					if hasTCP {
+						tuna.Close(tc.tcpConn[serviceId])
+						address := addressParts[0] + ":" + addressParts[1]
+						tc.tcpConn[serviceId], err = net.DialTimeout(
+							string(tuna.TCP),
+							address,
+							time.Duration(tc.config.DialTimeout)*time.Second,
+						)
+						if err != nil {
+							log.Println("Couldn't connect to TCP address", address, "from", subscriber, "because:", err)
+							continue RandomSubscriber
+						}
+						log.Println("Connected to TCP at", address, "from", subscriber)
+					}
+					if hasUDP {
+						tuna.Close(tc.udpConn[serviceId])
+						port, _ := strconv.Atoi(addressParts[2])
+						address := net.UDPAddr{IP: net.ParseIP(addressParts[0]), Port: port}
+						tc.udpConn[serviceId], err = net.DialUDP(
+							string(tuna.UDP),
+							nil,
+							&address,
+						)
+						if err != nil {
+							log.Println("Couldn't connect to UDP address", address, "from", subscriber, "because:", err)
+							continue RandomSubscriber
+						}
+						log.Println("Connected to UDP at", address, "from", subscriber)
 					}
 
-					if conn[serviceId] != nil {
-						tuna.Close(conn[serviceId])
-					}
-					conn[serviceId], err = net.DialTimeout(
-						string(protocol),
-						address,
-						time.Duration(tc.config.DialTimeout)*time.Second,
-					)
-					if err != nil {
-						log.Println("Couldn't connect to address", address, "from", subscriber, "because:", err)
-						continue RandomSubscriber
-					}
-
-					log.Println("Connected to proxy provider at", address, "from", subscriber)
 					break RandomBucket
 				}
 			}
 		}
 	}
 
-	return conn[serviceId], nil
+	return nil
 }
 
-func (tc *TunnelClient) getSession(serviceId byte, protocol tuna.Protocol, force bool) (*smux.Session, error) {
+func (tc *TunnelClient) getSession(serviceId byte, force bool) (*smux.Session, error) {
 	if tc.session[serviceId] == nil || tc.session[serviceId].IsClosed() || force {
-		conn, err := tc.connectToNode(serviceId, protocol, force)
+		conn, err := tc.getServerTCPConn(serviceId, force)
 		if err != nil {
 			return nil, err
 		}
@@ -148,21 +182,21 @@ func (tc *TunnelClient) getSession(serviceId byte, protocol tuna.Protocol, force
 	return tc.session[serviceId], nil
 }
 
-func (tc *TunnelClient) openStream(serviceId byte, portId byte, protocol tuna.Protocol, force bool) (*smux.Stream, error) {
-	session, err := tc.getSession(serviceId, protocol, force)
+func (tc *TunnelClient) openStream(serviceId byte, portId byte, force bool) (*smux.Stream, error) {
+	session, err := tc.getSession(serviceId, force)
 	if err != nil {
 		return nil, err
 	}
 	stream, err := session.OpenStream(serviceId, portId)
 	if err != nil {
-		return tc.openStream(serviceId, portId, protocol, true)
+		return tc.openStream(serviceId, portId, true)
 	}
 	return stream, err
 }
 
-func (tc *TunnelClient) listenService(serviceId byte, portIdOffset int, protocol tuna.Protocol, ports []int) {
+func (tc *TunnelClient) listenTCP(serviceId byte, ports []int) {
 	for i, port := range ports {
-		listener, err := net.Listen(string(protocol), ":"+strconv.Itoa(port))
+		listener, err := net.ListenTCP(string(tuna.TCP), &net.TCPAddr{Port: port})
 		if err != nil {
 			log.Panicln("Couldn't bind listener:", err)
 		}
@@ -175,7 +209,7 @@ func (tc *TunnelClient) listenService(serviceId byte, portIdOffset int, protocol
 					continue
 				}
 
-				stream, err := tc.openStream(serviceId, byte(portIdOffset+i), protocol, false)
+				stream, err := tc.openStream(serviceId, byte(i), false)
 				if err != nil {
 					log.Println("Couldn't open stream:", err)
 					tuna.Close(conn)
@@ -184,6 +218,90 @@ func (tc *TunnelClient) listenService(serviceId byte, portIdOffset int, protocol
 
 				go tuna.Pipe(stream, conn)
 				go tuna.Pipe(conn, stream)
+			}
+		}()
+	}
+}
+
+func (tc *TunnelClient) listenUDP(serviceId byte, portIdOffset int, ports []int) {
+	service := tuna.Services[serviceId]
+	hasUDP := len(service.UDP) > 0
+
+	if !hasUDP {
+		return
+	}
+
+	go func() {
+		buffer := make([]byte, 2048)
+		for {
+			serverConn, err := tc.getServerUDPConn(serviceId, false)
+			if err != nil {
+				log.Println("Couldn't get server connection:", err)
+				continue
+			}
+
+			n, err := serverConn.Read(buffer)
+			if err != nil {
+				log.Println("Couldn't receive data from server:", err)
+				continue
+			}
+
+			connKey := connKey{buffer[2], buffer[3]}
+			connId := *(*uint16)(unsafe.Pointer(&buffer[0]))
+
+			var serviceConn *net.UDPConn
+			var ok bool
+			if serviceConn, ok = tc.serviceConn[connKey]; !ok {
+				log.Println("Couldn't get service conn for:", connKey)
+				continue
+			}
+
+			var clientAddr *net.UDPAddr
+			if clientAddr, ok = tc.clientAddr[connId]; !ok {
+				log.Println("Couldn't get client address for:", connId)
+				continue
+			}
+
+			_, err = serviceConn.WriteToUDP(buffer[4:n], clientAddr)
+			if err != nil {
+				log.Println("Couldn't send data to client:", err)
+			}
+		}
+	}()
+
+	for i, port := range ports {
+		localConn, err := net.ListenUDP(string(tuna.UDP), &net.UDPAddr{Port: port})
+		if err != nil {
+			log.Panicln("Couldn't bind listener:", err)
+		}
+		portId := byte(i)
+		connKey := connKey{serviceId, portId}
+		tc.serviceConn[connKey] = localConn
+
+		go func() {
+			localBuffer := make([]byte, 2048)
+			for {
+				n, addr, err := localConn.ReadFromUDP(localBuffer)
+				if err != nil {
+					log.Println("Couldn't receive data from local:", err)
+					continue
+				}
+
+				connKey := uint16(addr.Port)
+				if _, ok := tc.clientAddr[connKey]; !ok {
+					tc.clientAddr[connKey] = addr
+				}
+
+				serverConn, err := tc.getServerUDPConn(serviceId, false)
+				if err != nil {
+					log.Println("Couldn't get remote connection:", err)
+					continue
+				}
+				connId := *(*[2]byte)(unsafe.Pointer(&addr.Port))
+				_, err = serverConn.Write(append([]byte{connId[0], connId[1], serviceId, portId}, localBuffer[:n]...))
+				if err != nil {
+					log.Println("Couldn't send data to server:", err)
+				}
 			}
 		}()
 	}
