75225ae2482d87d4c1529a14e7a267c4c592a40a
crasker <chouyc.adam@gmail.com>
2020-06-02 03:47:06 +0800

    remove redundant alias

--- bin/entry/main.go
+++ bin/entry/main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"github.com/jessevdk/go-flags"
-	nkn "github.com/nknorg/nkn-sdk-go"
+	"github.com/nknorg/nkn-sdk-go"
 	"github.com/nknorg/nkn/common"
 	"github.com/nknorg/tuna"
 )
