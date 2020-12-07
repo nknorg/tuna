a5f4dbb8987e174594511518d98c400e2c995192
Yilun <zyl.skysniper@gmail.com>
2020-12-06 22:29:07 -0800

    Fix connecting to node that fails latency measurement
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- tests/measurement_test.go
+++ tests/measurement_test.go
@@ -1,39 +1,33 @@
 package tests
 
 import (
-	"context"
-	"github.com/nknorg/tuna"
-	"github.com/nknorg/tuna/util"
 	"log"
+	"sync"
 	"testing"
 	"time"
+
+	"github.com/nknorg/tuna"
+	"github.com/nknorg/tuna/util"
 )
 
 var ips = []string{
-	"34.201.72.169:30010",
-	"167.71.88.124:30010",
-	"3.95.191.171:30010",
-	"54.202.225.44:30010",
+	"example.com:80",
 }
 
 func TestDelayMeasurement(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
 	for _, ip := range ips {
+		wg.Add(1)
 		go func(ip string) {
-			delay, err := util.DelayMeasurement(ctx, string(tuna.TCP), ip, time.Second*2)
+			defer wg.Done()
+			delay, err := util.DelayMeasurement(string(tuna.TCP), ip, time.Second*2)
 			if err != nil {
 				log.Println("timeout, ip:", ip)
 				return
 			}
 			log.Println("succeeded, ip:", ip, ", delay: ", delay)
-			cancel()
 		}(ip)
 	}
-
-	select {
-	case <-ctx.Done():
-		return
-	case <-time.After(10 * time.Second):
-		return
-	}
+	wg.Wait()
+	return
 }
