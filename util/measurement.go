3903d94a57e03e89c6dde3a1a12788c6781d9a25
heron <peng.liu@nkn.org>
2020-12-09 14:10:38 +0800

    add connection close when delay measurement

--- util/measurement.go
+++ util/measurement.go
@@ -8,11 +8,13 @@ import (
 func DelayMeasurement(network, address string, timeout time.Duration) (time.Duration, error) {
 	now := time.Now()
 	d := net.Dialer{Timeout: timeout}
-	_, err := d.Dial(network, address)
+	conn, err := d.Dial(network, address)
 	delay := time.Since(now)
 	if err != nil {
 		return delay, err
 	}
+
+	conn.Close()
 
 	return delay, nil
 }
