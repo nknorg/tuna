28671a066e938e7407e3ce1de4dc8a2462ad52b2
heron <peng.liu@nkn.org>
2020-12-17 15:01:31 +0800

    update ip filter use CIDR

---
+++ types/node.go
@@ -0,0 +1,33 @@
+package types
+
+import (
+	"github.com/nknorg/tuna/pb"
+	"time"
+)
+
+type Node struct {
+	Address   string
+	Metadata  *pb.ServiceMetadata
+	Delay     time.Duration
+	Bandwidth float32
+}
+type Nodes []*Node
+
+func (fs Nodes) Len() int {
+	return len(fs)
+}
+func (fs Nodes) Swap(i, j int) {
+	fs[i], fs[j] = fs[j], fs[i]
+}
+
+type SortByDelay struct{ Nodes }
+
+func (s SortByDelay) Less(i, j int) bool {
+	return s.Nodes[i].Delay < s.Nodes[j].Delay
+}
+
+type SortByBandwidth struct{ Nodes }
+
+func (s SortByBandwidth) Less(i, j int) bool {
+	return s.Nodes[i].Bandwidth > s.Nodes[j].Bandwidth
+}
