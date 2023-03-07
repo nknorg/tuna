bc5d239165daa5dbfbc744834c1cb37e98e33653
bufrr <chouyc.adam@gmail.com>
2023-03-07 15:39:18 +0800

    fix typo

--- config.go
+++ config.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	DefaultSubscriptionPrefix = "tuna_v111."
+	DefaultSubscriptionPrefix = "tuna_v1."
 	DefaultReverseServiceName = "reverse"
 
 	defaultNanoPayDuration                   = 4320 * 30
